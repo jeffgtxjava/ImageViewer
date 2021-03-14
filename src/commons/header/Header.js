700aa37074708d96f50e7bc004233db5a310fd61
jeffgtxjava <jeffgtxjava@gmail.com>
2021-03-14 22:41:00 +0530

    added dropdwon menu for profile pic

--- src/commons/header/Header.js
+++ src/commons/header/Header.js
@@ -1,9 +1,35 @@
 import React, { Component, Fragment } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import "./Header.css";
-import { Avatar, IconButton, Input, InputAdornment } from "@material-ui/core";
+import {
+  Avatar,
+  IconButton,
+  Input,
+  InputAdornment,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@material-ui/core";
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menuState: false,
+      anchorEl: null,
+    };
+  }
+
+  onProfileIconClick = (e) => {
+    this.setState({
+      menuState: !this.state.menuState,
+      anchorEl: e.currentTarget,
+    });
+  };
+
+  onMenuClose = () => {
+    this.setState({ menuState: !this.state.menuState, anchorEl: null });
+  };
   render() {
     return (
       <header className="app-header">
@@ -12,7 +38,7 @@ class Header extends Component {
             <span className="header-logo-text">Image Viewer</span>
           </div>
         ) : (
-          <div>
+          <>
             <span className="header-logo-text">Image Viewer</span>
             <Fragment>
               <div>
@@ -30,15 +56,33 @@ class Header extends Component {
                     </InputAdornment>
                   }
                 />
-                <IconButton>
+                <IconButton id="profile-icon" onClick={this.onProfileIconClick}>
                   <Avatar
                     alt="profile_picture"
                     src={this.props.profilePictureUrl}
                   />
                 </IconButton>
+                <div>
+                  <Menu
+                    open={this.state.menuState}
+                    onClose={this.onMenuClose}
+                    anchorEl={this.state.anchorEl}
+                    getContentAnchorEl={null}
+                    anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                    keepMounted
+                  >
+                    <MenuItem>
+                      <Typography>My Account</Typography>
+                    </MenuItem>
+                    <hr className="horizontal-line" />
+                    <MenuItem>
+                      <Typography>Logout</Typography>
+                    </MenuItem>
+                  </Menu>
+                </div>
               </div>
             </Fragment>
-          </div>
+          </>
         )}
       </header>
     );
